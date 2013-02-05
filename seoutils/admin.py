b50d76287b59bee3b607f4443d5e0b67bdb77860
h3 <max@motion-m.ca>
2013-02-05 11:29:38 -0500

    Translate only if modeltranslation is in INSTALLED_APPS

--- seoutils/admin.py
+++ seoutils/admin.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.conf import settings
+
 from seoutils.models import Meta
 
 #class PageMetaAdmin(admin.ModelAdmin):
 #    list_display   = ('site_url', 'title', 'keywords', 'desc',)
 #admin.site.register(PageMeta, PageMetaAdmin)
 
-
-
-try:
-    from grappellifit.admin import TranslationAdmin
-    BaseAdmin = TranslationAdmin
-except:
+if 'modeltranslation' in settings.INSTALLED_APPS:
     try:
-        from modeltranslation.admin import TranslationAdmin
+        from grappellifit.admin import TranslationAdmin
         BaseAdmin = TranslationAdmin
     except:
-        BaseAdmin = admin.ModelAdmin
-
+        try:
+            from modeltranslation.admin import TranslationAdmin
+            BaseAdmin = TranslationAdmin
+        except:
+            BaseAdmin = admin.ModelAdmin
+else:
+    BaseAdmin = admin.ModelAdmin
 
 class MetaAdmin(BaseAdmin):
     list_display   = ('path_info', 'title', 'keywords', 'desc',)
