04b23686f6c342c0ac4a85dadfcb86c0661e2e20
Maxime Haineault <max@motion-m.ca>
2013-05-06 12:08:02 -0400

    Added french translations, cleanup in translations

--- seoutils/models.py
+++ seoutils/models.py
@@ -7,15 +7,15 @@ from django.core.urlresolvers import resolve
 from django.utils.translation import gettext_lazy as _
 
 class Meta(models.Model):
-    path_info = models.CharField(u"URL de la page", max_length=250, unique=True, 
+    path_info = models.CharField(_('URL of the page'), max_length=250, unique=True, 
             help_text=_('This can be a URL or a URL name. Ex.: "/contact/" or "website-contact"'))
-    title     = models.CharField(u"Titre de page", max_length=250, blank=True, null=True, 
+    title     = models.CharField(_('Page title'), max_length=250, blank=True, null=True, 
             help_text=_('A page title. It will show up in search engine search results.'))
-    keywords  = models.CharField(u"Mots clés", max_length=250, blank=True, null=True, 
+    keywords  = models.CharField(_('Keywords'), max_length=250, blank=True, null=True, 
             help_text=_('Keywords separated by spaces.'))
-    desc      = models.TextField(u"Description", blank=True, null=True, 
+    desc      = models.TextField(_('Description'), blank=True, null=True, 
             help_text=_('A short description for the page. This will show up in search engine search results and it should not be longer than 150 characters.'))
-    extra_js  = models.TextField(u"JavaScript", blank=True, null=True, 
+    extra_js  = models.TextField(_('JavaScript'), blank=True, null=True, 
             help_text=_('Extra JavaScript to inject into page, for example campaign tracking code (script tags must be included).'))
 
     def __unicode__(self):
