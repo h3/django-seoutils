51e80e929316bea3d95832266498a8bbf11eb592
Maxime Haineault <max@motion-m.ca>
2013-05-06 12:17:26 -0400

    Static analysis and pep8 completed for models

--- seoutils/models.py
+++ seoutils/models.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
-from django.core.urlresolvers import resolve
 from django.utils.translation import gettext_lazy as _
 
+
 class Meta(models.Model):
-    path_info = models.CharField(_('URL of the page'), max_length=250, unique=True, 
-            help_text=_('This can be a URL or a URL name. Ex.: "/contact/" or "website-contact"'))
-    title     = models.CharField(_('Page title'), max_length=250, blank=True, null=True, 
-            help_text=_('A page title. It will show up in search engine search results.'))
-    keywords  = models.CharField(_('Keywords'), max_length=250, blank=True, null=True, 
-            help_text=_('Keywords separated by spaces.'))
-    desc      = models.TextField(_('Description'), blank=True, null=True, 
-            help_text=_('A short description for the page. This will show up in search engine search results and it should not be longer than 150 characters.'))
-    extra_js  = models.TextField(_('JavaScript'), blank=True, null=True, 
-            help_text=_('Extra JavaScript to inject into page, for example campaign tracking code (script tags must be included).'))
+    path_info = models.CharField(_('URL of the page'), max_length=250,
+            unique=True, help_text=_('This can be a URL or a URL name. \
+Ex.: "/contact/" or "website-contact"'))
+
+    title = models.CharField(_('Page title'), max_length=250, blank=True,
+            null=True, help_text=_('A page title. It will show up in search \
+engine search results.'))
+
+    keywords = models.CharField(_('Keywords'), max_length=250, blank=True,
+            null=True, help_text=_('Keywords separated by spaces.'))
+
+    desc = models.TextField(_('Description'), blank=True, null=True,
+            help_text=_('A short description for the page. This will show up \
+in search engine search results and it should not be longer \
+than 150 characters.'))
+
+    extra_js = models.TextField(_('JavaScript'), blank=True, null=True,
+            help_text=_('Extra JavaScript to inject into page, for example \
+campaign tracking code (script tags must be included).'))
 
     def __unicode__(self):
         return _("SEO meta for %(path)s") % {'path': self.path_info}
+
+
+class Analytic(models.Model):
+    is_active = models.BooleanField(_('Is active'), default=True)
+    code = models.TextField(_('Code JavaScript'))
+    description = models.CharField(_('Description'), max_length=250,
+            blank=True, null=True, default='Main Analytic',
+            help_text=_('An optional description used only for internal \
+reference.'))
